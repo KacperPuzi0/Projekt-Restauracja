5e9dace5d2390dd681c3d555be18732cee91f972
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-05-30 20:47:51 +0200

    ++

--- src/src/index.ts
+++ src/src/index.ts
@@ -1,2 +1,20 @@
 import express from "express";
 import { connectToDatabase } from "./services/database.service";
+import { danieRouter} from "./routes/danie.router";
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+connectToDatabase()
+    .then(() => {
+        app.use("/danie", danieRouter);
+
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
