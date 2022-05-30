46caac8dde2e4c0f8243d3823a45cf676603d1ce
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-05-30 20:38:28 +0200

    DanieRouter Done

--- src/src/routes/danie.router.ts
+++ src/src/routes/danie.router.ts
@@ -20,4 +20,83 @@ danieRouter.get("/", async (req: Request, res: Response) => {
 	}
 });
 
+danieRouter.get("/:id", async (req: Request, res: Response) => {
+	try {
+		const danie = (await collections.Danie.findOne({ _id: new ObjectId(req.params.id) })) as Danie;
+		if(danie){
+			res.status(200).send(danie);
+		}else {
+			res.status(404).send("Nie znaleziono dania");
+		}
+		
+
+	}catch (error) {
+		let errorMessage = "Błąd podczas pobierania dania";
+		if (error.message) {
+			errorMessage = error.message;
+		}
+		res.status(500).send(error.message);
+	}
+});
+
+danieRouter.post("/", async (req: Request, res: Response) => {
+	try {
+		const danie = req.body as Danie;
+		const result = await collections.Danie.insertOne(danie);
+
+		result ? res.status(201).send(`Dodano danie: ${result.insertedId}`) 
+		: res.status(500).send("Błąd podczas dodawania dania");
+
+	}catch (error) {
+		let errorMessage = "Błąd podczas dodawania dania";
+		if (error.message) {
+			errorMessage = error.message;
+		}
+		res.status(500).send(error.message);
+	}
+});
+
+danieRouter.put("/:id", async (req: Request, res: Response) => {
+	const id = req?.params?.id;
+	try{
+		const updateDanie: Danie = req.body as Danie;
+		const query = { _id: new ObjectId(id) };
+
+		const result = await collections.Danie.updateOne(query, {$set: updateDanie});
+
+		result ? res.status(200).send(`Zaktualizowano danie: ${id}`)
+		: res.status(500).send(`Błąd podczas aktualizacji dania ${id}`);
+	}catch (error){
+		let errorMessage = "Błąd podczas aktualizacji dania";
+		if (error.message) {
+			errorMessage = error.message;
+		}
+		res.status(400).send(error.message);
+
+	}
+});
+
+danieRouter.delete("/:id", async (req: Request, res: Response) => {
+	const id = req?.params?.id;
+	try{
+		const query = { _id: new ObjectId(id) };
+		const result = await collections.Danie.deleteOne(query);
+
+		if(result && result.deletedCount){
+			res.status(202).send(`Usunięto danie: ${id}`);
+		} else if (!result){
+			res.status(404).send(`Nie znaleziono dania: ${id}`);
+		} else if (!result.deletedCount){
+			res.status(500).send(`Błąd podczas usuwania dania ${id}`);
+		}
+	}catch (error){
+		let errorMessage = "Błąd podczas usuwania dania";
+		if (error.message) {
+			errorMessage = error.message;
+		}
+		res.status(400).send(error.message);
+
+	}
+});
+
 
